60eccca2760e6619b72810ccd8be0506c649a575
mrimon93 <muzammil_kamaly93@live.se>
2023-12-29 21:53:54 +0100

    finished with controller, remmeber to journal

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -14,15 +14,44 @@ public class TodoController : Controlle
     }
 
     [HttpGet]
-
+    public IEnumerable<Todo> GetTodos()
+    {
+        return await todoRepository.GetTodos();
+    }
 
     [HttpGet("{id}")]
+    public async Task<Todo> GetTodoById.GetTodos([FromRoute] int id)
+    {
+        return await todoRepository.GetTodoById();
+    }
 
 
     [HttpPost]
+    public async Task<IActionResult> AddTodo([FromRoute] Todo todo)
+    {
+        await todoRepository.AddTodo();
+        return CreatedAtActionResult("GetTodoById", new {id = todo.Id}, todo);
+    }
 
 
     [HttpPut["{id}"]]
+    public async Task<IActionResult> UpdateTodo([FromRoute] int id, [FromRoute] Todo updateTodo)
+    {
+        if (id != updateTodo.Id)
+        {
+           return BadRequest("The ID of the updated todo does not match the ID in the URL");
+        }
+
+        await todoRepository.UpdateTodo(updateTodo);
+        return NoContent();
+    }
 
     [HttpDelete["{id}"]]
+
+    public async Task<IActionResult> DeleteTodo([FromRoute] int id)
+    {
+        await todoRepository.DeleteTodo(id);
+        return NoContent();
+    }
+
 }
