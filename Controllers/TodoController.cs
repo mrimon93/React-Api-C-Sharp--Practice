186038f3df0a858c687b6358915e3a9cd3d3e081
mrimon93 <muzammil_kamaly93@live.se>
2023-12-29 21:44:41 +0100

    issue with git recommiting

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Models;
 using TodoApi.Repositories;
@@ -27,33 +26,3 @@ public class TodoController : Controlle
 
     [HttpDelete["{id}"]]
 }
-=======
-using Microsoft.AspNetCore.Mvc;
-using TodoApi.Models;
-using TodoApi.Repositories;
-
-
-[Route("api/[controller]")]
-public class TodoController : Controlle
-{
-    private readonly ITodoRepository todoRepository;
-
-    public TodoController(ITodoRepository todoRepository)
-    {
-        this.todoRepository = todoRepository;
-    }
-
-    [HttpGet]
-
-
-    [HttpGet("{id}")]
-
-
-    [HttpPost]
-
-
-    [HttpPut["{id}"]]
-
-    [HttpDelete["{id}"]]
-}
->>>>>>> 0637ed46afc98d2071a8c97f81629647b5b96de0
